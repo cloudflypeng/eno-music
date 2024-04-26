41690787cb40cbeb46e38d1ea7eff1a8a5c69d90
cloudflypeng <44285412+cloudflypeng@users.noreply.github.com>
2024-04-26 16:23:37 +0800

    Initial commit

---
+++ src/background/main.ts
@@ -0,0 +1,54 @@
+import { onMessage, sendMessage } from 'webext-bridge/background'
+import type { Tabs } from 'webextension-polyfill'
+
+// only on dev mode
+if (import.meta.hot) {
+  // @ts-expect-error for background HMR
+  import('/@vite/client')
+  // load latest content script
+  import('./contentScriptHMR')
+}
+
+browser.runtime.onInstalled.addListener((): void => {
+  // eslint-disable-next-line no-console
+  console.log('Extension installed')
+})
+
+let previousTabId = 0
+
+// communication example: send previous tab title from background page
+// see shim.d.ts for type declaration
+browser.tabs.onActivated.addListener(async ({ tabId }) => {
+  if (!previousTabId) {
+    previousTabId = tabId
+    return
+  }
+
+  let tab: Tabs.Tab
+
+  try {
+    tab = await browser.tabs.get(previousTabId)
+    previousTabId = tabId
+  }
+  catch {
+    return
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('previous tab', tab)
+  sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
+})
+
+onMessage('get-current-tab', async () => {
+  try {
+    const tab = await browser.tabs.get(previousTabId)
+    return {
+      title: tab?.title,
+    }
+  }
+  catch {
+    return {
+      title: undefined,
+    }
+  }
+})
