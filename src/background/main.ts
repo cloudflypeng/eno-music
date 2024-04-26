e6ab30b79b61a66c68ef5c7c41acf4c6393a422d
pengyunfei <pengyunfei@360.cn>
2024-04-26 18:31:30 +0800

    Add pinia dependency and update cSpell.words in settings.json

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,54 +1,28 @@
-import { onMessage, sendMessage } from 'webext-bridge/background'
-import type { Tabs } from 'webextension-polyfill'
+// import browser from 'webextension-polyfill'
 
-// only on dev mode
-if (import.meta.hot) {
-  // @ts-expect-error for background HMR
-  import('/@vite/client')
-  // load latest content script
-  import('./contentScriptHMR')
+import { apiListenerFactory } from './utils'
+
+import API_BILIBILI from './api/bilibili'
+
+// Merge all API objects into one
+const FullAPI = Object.assign({}, API_BILIBILI)
+// Create a message listener for each API
+// const handleMessage = apiListenerFactory(FullAPI)
+const apiProxy = apiListenerFactory(FullAPI)
+export {
+  apiProxy,
 }
+// function handleConnect() {
+//   browser.runtime.onMessage.removeListener(handleMessage)
+//   browser.runtime.onMessage.addListener(handleMessage)
+// }
 
-browser.runtime.onInstalled.addListener((): void => {
-  // eslint-disable-next-line no-console
-  console.log('Extension installed')
-})
+// (function startApi() {
+//   browser.runtime.onConnect.removeListener(handleConnect)
+//   browser.runtime.onConnect.addListener(handleConnect)
+// })()
 
-let previousTabId = 0
-
-// communication example: send previous tab title from background page
-// see shim.d.ts for type declaration
-browser.tabs.onActivated.addListener(async ({ tabId }) => {
-  if (!previousTabId) {
-    previousTabId = tabId
-    return
-  }
-
-  let tab: Tabs.Tab
-
-  try {
-    tab = await browser.tabs.get(previousTabId)
-    previousTabId = tabId
-  }
-  catch {
-    return
-  }
-
-  // eslint-disable-next-line no-console
-  console.log('previous tab', tab)
-  sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
-})
-
-onMessage('get-current-tab', async () => {
-  try {
-    const tab = await browser.tabs.get(previousTabId)
-    return {
-      title: tab?.title,
-    }
-  }
-  catch {
-    return {
-      title: undefined,
-    }
-  }
-})
+// browser.runtime.onInstalled.addListener((): void => {
+//   // eslint-disable-next-line no-console
+//   console.log('Extension installed')
+// })
--- src/options/main.ts
+++ src/options/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './Options.vue'
 import { setupApp } from '~/logic/common-setup'
 import '../styles'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+
 setupApp(app)
 app.mount('#app')
