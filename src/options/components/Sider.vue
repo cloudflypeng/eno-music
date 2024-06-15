497e16918ccc904ce6e1a7725a0c766942895163
pengyunfei <pengyunfei@360.cn>
2024-06-15 19:54:40 +0800

    chore: Update Sider.vue with hover effects for icons

--- src/options/components/Sider.vue
+++ src/options/components/Sider.vue
@@ -8,13 +8,16 @@ const store = useBlblStore()
   <aside w-20 h-full flex flex-col gap-8 items-center py-30 text-3xl>
     <div
       class="i-tabler:smart-home w-1em h-1em"
+      hover:scale-110
       title="返回" @click.stop="store.mode = 'home'"
     />
     <div
+      hover:scale-110
       class="i-tabler:music-search w-1em h-1em"
       @click.stop="store.mode = 'search'"
     />
     <div
+      hover:scale-110
       class="i-streamline:play-list-folder w-1em h-1em"
       @click.stop="store.mode = 'playlist'"
     />
