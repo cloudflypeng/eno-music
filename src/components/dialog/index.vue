cb0be16f1650f4e7435542b87b768d58e3397801
pengyunfei <cloudflypeng@gmail.com>
2024-07-07 20:02:34 +0800

    feat: 添加对话框组件并在相关页面使用
    为项目添加了一个通用的对话框组件，并在Play.vue和AddSong.vue页面中使用该组件来替换原有的代码。这样可以提高代码的复用性和可维护性，并且使页面结构更加清晰。

---
+++ src/components/dialog/index.vue
@@ -0,0 +1,50 @@
+<script setup>
+// open 用v-model绑定
+const props = defineProps({
+  open: Boolean,
+  title: String,
+})
+const emit = defineEmits(['visibleChange'])
+const dialogRef = ref(null)
+
+watch(() => props.open, (open) => {
+  if (open)
+    dialogRef.value.showModal()
+  else
+    dialogRef.value.close()
+})
+onMounted(() => {
+  dialogRef.value.addEventListener('close', () => {
+    emit('visibleChange', false)
+  })
+})
+function close() {
+  emit('visibleChange', false)
+}
+</script>
+
+<template>
+  <Teleport to="body">
+    <dialog
+      v-show="open" ref="dialogRef"
+      class="bg-$eno-elevated backdrop-blur p-3 rounded-lg h-[50vh] min-w-1/3 max-w-1/2
+        text-white
+      "
+    >
+      <div
+        text-3xl mb-2
+        class="flex justify-between"
+      >
+        <div>{{ props.title }}</div>
+        <div
+          class="i-mingcute:close-fill w-1em h-1em items-center transition-all
+          transition-delay-300 hover:transform-rotate-90 cursor-pointer"
+          @click.stop="close"
+        />
+      </div>
+      <div class="max-h-[calc(100%-3rem)] max-w-[50vw] overflow-auto">
+        <slot />
+      </div>
+    </dialog>
+  </Teleport>
+</template>
