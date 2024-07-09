1d2fdfedd579e4b4c12566240e6c89725cb224bd
pengyunfei <cloudflypeng@gmail.com>
2024-07-09 09:57:18 +0800

    feat: 添加点击对话框外部关闭功能

--- src/components/dialog/index.vue
+++ src/components/dialog/index.vue
@@ -21,6 +21,12 @@ onMounted(() => {
 function close() {
   emit('visibleChange', false)
 }
+function clickDialog(e) {
+  const { clientX, clientY } = e
+  const { left, top, right, bottom } = dialogRef.value.getBoundingClientRect()
+  if (clientX < left || clientX > right || clientY < top || clientY > bottom)
+    close()
+}
 </script>
 
 <template>
@@ -30,20 +36,23 @@ function close() {
       class="bg-$eno-elevated backdrop-blur p-3 rounded-lg h-[50vh] min-w-1/3 max-w-1/2
         text-white
       "
+      @click="clickDialog"
     >
-      <div
-        text-3xl mb-2
-        class="flex justify-between"
-      >
-        <div>{{ props.title }}</div>
+      <div>
         <div
-          class="i-mingcute:close-fill w-1em h-1em items-center transition-all
+          text-3xl mb-2
+          class="flex justify-between"
+        >
+          <div>{{ props.title }}</div>
+          <div
+            class="i-mingcute:close-fill w-1em h-1em items-center transition-all
           transition-delay-300 hover:transform-rotate-90 cursor-pointer"
-          @click.stop="close"
-        />
-      </div>
-      <div class="max-h-[calc(100%-3rem)] max-w-[50vw] overflow-auto">
-        <slot />
+            @click.stop="close"
+          />
+        </div>
+        <div class="max-h-[calc(100%-3rem)] max-w-[50vw] overflow-auto">
+          <slot />
+        </div>
       </div>
     </dialog>
   </Teleport>
