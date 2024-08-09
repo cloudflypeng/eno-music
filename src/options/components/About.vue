524ac8ebf94554f3a9cf2e62fa0978798aac5d68
pengyunfei <cloudflypeng@gmail.com>
2024-08-09 10:15:54 +0800

    feat: 增加爱发电

--- src/options/components/About.vue
+++ src/options/components/About.vue
@@ -22,12 +22,17 @@ const btn = 'inline-block flex items-center gap-1 text-lg rounded-4 px-4 py-2 bg
         <div class="i-mingcute:qq-line w-1em h-1em" />
         <span>群:762231762</span>
       </a>
+      <a href="https://afdian.com/a/meanc" :class="`${btn} bg-[#946ce6]`">
+        <div class="i-mingcute:flash-line w-1em h-1em" />
+        爱发电
+      </a>
     </div>
     <!-- info -->
     <span class="flex flex-col text-lg inline-block mb-25">
       <li>本项目仅供学习交流使用，不得用于商业用途</li>
       <li>使用歌手功能, 需要当前浏览器登录过bilibili</li>
       <li>需要交流反馈可以添加qq群和discord, 长期在线</li>
+      <li>🫡🫡统计用户达到30000 or 日活达到1000 or 爱发电赞助破千 开启手机版移植</li>
     </span>
   </section>
 </template>
