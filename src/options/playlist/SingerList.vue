a6807d2a80d69d7c50631530381c5d33f92ba2b3
cloudflypeng <cloudflypeng@gmail.com>
2024-10-25 12:09:02 +0800

    feat: 增加通过空间链接添加歌手

--- src/options/playlist/SingerList.vue
+++ src/options/playlist/SingerList.vue
@@ -2,6 +2,18 @@
 import Dialog from '../../components/dialog/index.vue'
 import SingerCard from '../components/SingerCard.vue'
 import { defaultSingers, usePlaylistStore } from './store'
+// https://space.bilibili.com/17819768?spm_id_from=333.1007.tianma.1-1-1.click
+function getMidFromUrl(url) {
+  // 如果url 是纯数字, 则直接返回
+  if (/^\d+$/.test(url)) {
+    return url
+  }
+  const match = url.match(/space\.bilibili\.com\/(\d+)/)
+  if (match) {
+    return match[1]
+  }
+  return ''
+}
 
 const PLstore = usePlaylistStore()
 onMounted(() => {
@@ -13,12 +25,13 @@ const dialogVis = ref(false)
 const singerMid = ref('')
 function addSinger() {
   // 判断是否已经存在, 判断singerMid是否合法
-  if (!singerMid.value)
+  const mid = getMidFromUrl(singerMid.value)
+  if (!mid)
     return
-  if (defaultSingers.includes(singerMid.value) || PLstore.singers.includes(singerMid.value))
+  if (defaultSingers.includes(mid) || PLstore.singers.includes(mid))
     return
 
-  PLstore.addSinger(singerMid.value)
+  PLstore.addSinger(mid)
   dialogVis.value = false
 }
 </script>
@@ -46,8 +59,8 @@ function addSinger() {
         <Dialog :open="dialogVis" title="添加自定义歌手" @visible-change="dialogVis = $event">
           <div class="flex flex-col gap-3 w-full h-full justify-between">
             <input
-              v-model="singerMid" placeholder="请输入歌手mid" bg="$eno-content focus:$eno-content-hover"
-              class="h-10 px-3 border border-gray-200 rounded-4 m-5"
+              v-model="singerMid" placeholder="请输入歌手mid或up主主页链接" bg="$eno-content focus:$eno-content-hover"
+              class="h-10 px-3 border border-gray-200 rounded-4"
             >
             <button class="w-full h-10 bg-$eno-fill-2 text-white rounded-4" @click="addSinger">
               添加
